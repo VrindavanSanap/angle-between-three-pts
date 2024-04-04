c76d01b95d1588e0e61fbfeb6ff60787b1cc5613
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-04-04 12:45:43 +0530

    1

--- js/point.js
+++ js/point.js
@@ -1,3 +1,20 @@
+function get_angle(point1, point2, point3 = false) {
+    let angle = 0;
+    if (!point3) {
+        let dy = point1.y - point2.y;
+        let dx = point1.x - point2.x;
+        angle = Math.atan2(dy, dx);
+        angle = rad_to_deg(angle);
+        if (angle < 0) {
+            angle += 360;
+        }
+    } else {
+        let angle1 = get_angle(p1, p2);
+        let angle2 = get_angle(p2, p3);
+        angle = 180 + (angle2 - angle1);
+    }
+    return angle;
+}
 class Point {
     constructor(width_ = 0, height_ = 0, x = Math.random() * width_, y = Math.random() * height_) {
         this.x = x;
@@ -21,10 +38,13 @@ class Point {
         this.x = x
         this.y = y
     }
-    angle(prev1, prev2) {
-        let angle = Math.atan2(prev1.y - prev1.y, prev2.x - prev2.x);
-        angle = rad_to_deg(angle)
-        return angle;
+    get_angle(prev1, prev2=false) {
+        if (!prev2) {
+           return get_angle(prev1, this) 
+        } else {
+            return get_angle(prev2, prev1, this)
+        }
+
     }
     update(mouse_x, mouse_y, mouse_is_pressed) {
         let distance = dist(mouse_x, mouse_y, this.x, this.y);
